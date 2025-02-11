a346730b2f6ddffbe5cae75813c0db5ee58e478f
Dallas <beta68@gmail.com>
2025-02-11 11:52:56 -0600

    haskell nix build

---
+++ haskell_nix_build.sh
@@ -0,0 +1,10 @@
+sh <(curl -L https://nixos.org/nix/install) --daemon && \
+sudo nix-channel --add https://nixos.org/channels/nix-unstable &&\
+reset && \
+sudo nix-shell --packages 'ghc.withPackages (pkgs: [ networking ])' && \
+ghc -threaded share.hs && \
+exit
+
+
+
+
